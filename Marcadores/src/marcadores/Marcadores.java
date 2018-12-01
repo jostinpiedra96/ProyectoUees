5f2e20aa7e8d920a6ef15bfc5687da6b81578799
japied <japied@LAECP-ELPT20017.aceins.com>
2018-11-30 19:14:01 -0500

    Probando el commit del GitHub.

---
+++ Marcadores/src/marcadores/Marcadores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marcadores;
+
+/**
+ *
+ * @author japied
+ */
+public class Marcadores {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal p = new Principal();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+    }
+    
+}
